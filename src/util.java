0380fda207ad7535d8cd33c43535841d1948839d
gene-999 <eugenebaidoo5@gmail.com>
2024-07-13 01:27:05 +0000

    utilities used exclusively for djikstra implementation

--- src/util.java
+++ src/util.java
@@ -1,93 +1,68 @@
 package src;
-import java.util.*;
-    public class Node{
+
+import java.util.ArrayList;
+import java.util.List;
+
+public  class util {
+    static class Node {
         public int state;
-        public Integer parent;
+        public Node parent;
         public int distanceFromParent;
-        public Node(int state, Integer parent, int distanceFromDistance){
+
+        public Node(int state, Node parent, int distanceFromParent) {
             this.state = state;
             this.parent = parent;
-            this.distanceFromParent = distanceFromDistance;
-        }
-
-    }
-
-static class Queue {
-
-
-    int front = -1;
-    int back = -1;
-
-    List<Integer> items = new ArrayList<>();
-    public boolean isFull() {
-        return back == items.size() - 1;
-    }
-
-    public boolean isEmpty() {
-        return front == -1 && back == -1;
-    }
-
-
-    public void enQueue(int itemValue) {
-        if (isFull()) {
-            System.out.println("Queue is full");
-        } else if (isEmpty()) {
-            front = back = 0;
-            items.set(back, itemValue);
-        } else {
-            back++;
-            items.set(back, itemValue);
+            this.distanceFromParent = distanceFromParent;
         }
     }
 
-    public int deQueue() {
-        if (isEmpty()) {
-            System.out.println("Queue is empty. Nothing to dequeue");
-        } else if (front == back) {
-            front = back = -1;
-        } else {
-            front++;
+    static class QueueFrontier {
+        List<Node> items = new ArrayList<>();
+
+
+        public boolean isEmpty() {
+            return items.isEmpty();
         }
-        return items.get(front);
+
+        public void enQueue(Node item) {
+
+            for(Node node: items){
+                if(node.distanceFromParent > item.distanceFromParent){
+                    items.add(items.indexOf(node), item);
+                    return;
+                }
+            }
+                items.add(item);
+        }
+
+        public Node deQueue() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty. Nothing to dequeue");
+                return null;
+            }
+                Node item = items.get(0);
+                items.remove(0);
+                return item;
+
+        }
+
+        public boolean contains(int value) {
+            for (int i = 0; i <= items.size(); i++) {
+                if (items.get(i).state == value) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
-    public void display() {
-        int i;
-
-        if (isEmpty()) {
-            System.out.println("Queue is empty");
-        } else {
-            for (i = front; i <= back; i++) {
-                System.out.println(items.get(i));
+    public static List<Integer> neighboursOfNode(int[][] graph, int node) {
+        List<Integer> neighbours = new ArrayList<>();
+        for (int i = 0; i < graph[node].length; i++) {
+            if (graph[node][i] != 0) {
+                neighbours.add(i);
             }
         }
+        return neighbours;
     }
-
-    public void peak() {
-        System.out.println("Front value is: " + items.get(front));
-    }
-
-    public void clear() {
-        front = back = -1;
-    }
-
-    public boolean contains(int value){
-        for(int i = front; i <= back; i++){
-            if(items.get(i) == value){
-                return true;
-            }
-        }
-        return false;
-    }
-}
-
-public List<Integer> neighboursOfNode(int [][] graph, int node){
-    List<Integer> neighbours = new ArrayList<>();
-    for(int i = 0; i < graph.length; i++){
-        if(graph[node][i] != 0){
-            neighbours.add(i);
-        }
-    }
-    return neighbours;
-}
 }
