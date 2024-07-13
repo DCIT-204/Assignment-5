e37f4ba201ad49d6e041f74e7c81ab981d5a0428
SelinaKudadze11105718 <seakudadze001@st.ug.edu.gh>
2024-07-13 13:31:50 +0000

    Node class and constructors created

--- src/HuffmanCodes.java
+++ src/HuffmanCodes.java
@@ -11,4 +11,33 @@ public class HuffmanCodes {
         }
         return frequency;
     }
+
+    //Node class with two constructors
+    static class Node implements Comparable<Node>{
+        //attributes of Node class
+        int frequency;
+        char character;
+        Node left;
+        Node right;
+
+        //1ST constructor initializes frequency and character attributes
+        public Node(char character,int frequency){
+            this.character=character;
+            this.frequency=frequency;
+        }
+
+        //2ND constructor initializes frequency,left and right Nodes
+        public Node(int frequency,Node left,Node right){
+            this.frequency=frequency;
+            this.left=left;
+            this.right=right;
+        }
+
+        //compareTo method that aids priority queue to sort/order Nodes
+        @Override
+        public int compareTo(Node otherNode) {
+            return this.frequency-otherNode.frequency;
+        }
+
+    }
 }
