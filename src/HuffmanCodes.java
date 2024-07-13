c26de1b39717fb37a02c02feb4f9d7efc8cf2790
SelinaKudadze11105718 <seakudadze001@st.ug.edu.gh>
2024-07-13 13:24:46 +0000

    Method for obtaining frequency of each character in the input string created

--- src/HuffmanCodes.java
+++ src/HuffmanCodes.java
@@ -1,4 +1,14 @@
 package src;
+import java.util.HashMap;
 
 public class HuffmanCodes {
+    //method obtains frequencies of each character
+    public HashMap<Character,Integer> getFrequency(String inputText){
+        HashMap<Character,Integer> frequency = new HashMap<>();
+
+        for(char character: inputText.toCharArray()){
+            frequency.put(character,frequency.getOrDefault(character,0)+1);
+        }
+        return frequency;
+    }
 }
